5d5ff3bea7ea779aec446bbddaa310ada046103f
shenshuo <shenshuo@shinezone.com>
2019-01-07 16:53:56 +0800

    优化sql优化的输出

--- exec_sqladvisor.py
+++ exec_sqladvisor.py
@@ -17,13 +17,11 @@ from api_handler import API
 def get_conf(dbname, db_info):
     """获取SQL配置生产配置文件"""
     conf_file = '/tmp/' + uuid() + '.ini'
-    print("conf_file  is " + conf_file)
     if len(str(base64.b64decode(db_info)).split(',,,')) < 4:
         print('db_info error')
         exit(-100)
 
     db_host, db_port, db_user, db_pwd = str(base64.b64decode(db_info), 'utf-8').split(',,,')
-    print(db_host, db_port, db_user)
     if db_pwd == 'null':
         db_pwd = ''
     with open(conf_file, 'w') as f:
@@ -34,26 +32,21 @@ def get_conf(dbname, db_info):
                 "port={db_port}\n"
                 "dbname={dbname}\n"
                 .format(db_user=db_user, db_pwd=db_pwd, db_host=db_host, db_port=db_port, dbname=dbname))
-    return conf_file
+    return conf_file, db_host
 
 
-def exec_sqladvisor_v2(db_name, conf_file, sqls, mail_to):
-    print('exec db is {}'.format(db_name))
+def exec_sqladvisor_v2(conf_file, sqls):
+    all_result = ''
 
     for s in sqls.split(';'):
         if s:
             sub2 = subprocess.Popen('sqladvisor -f {} -q "{};" -v 1'.format(conf_file, s), shell=True,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.STDOUT)
+                                    stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             stdout, stderr = sub2.communicate()
             result = stdout.decode('utf-8').replace('\n\n', '\n')
-            print(result)
-            ### 发送邮件
-            msg = "SQL优化完毕！\n库名：{}\n语句：{}\n详情：{}".format(db_name, sqls, result)
-            obj = API()
+            all_result = '{}\n{}'.format(all_result, result)
 
-            res = obj.send_mail_for_api(mail_to, 'SQL索引优化', msg)
-            print(res)
+    return all_result
 
 
 def main(publish_name, db_name, sqls):
@@ -66,14 +59,26 @@ def main(publish_name, db_name, sqls):
     publish_hosts_api, mail_to = result[0]['publish_hosts_api'], result[0]['mail_to']
 
     print(publish_hosts_api, mail_to)
+    all_msg = ''
     if publish_hosts_api:
         res = obj.get_api_info(publish_hosts_api)
         for db in res['db_list']:
             if db['db_type'] == 'MySQL' and db['db_role'] == 'master':
-
                 db_info = db['db_info']
-                conf_file = get_conf(db_name, db_info)
-                exec_sqladvisor_v2(db_name, conf_file, sqls, mail_to)
+                conf_file, db_host = get_conf(db_name, db_info)
+                print('db host is {}'.format(db_host))
+                print("conf_file  is " + conf_file)
+                all_msg = all_msg + 'db host is {}\n'.format(db_host)
+                all_msg = all_msg + "conf_file is {}\n".format(conf_file)
+                result_msg = exec_sqladvisor_v2(conf_file, sqls)
+                print(result_msg)
+                all_msg = all_msg + result_msg
+
+        ### 发送邮件
+        msg = "SQL优化完毕！\n库名：{}\n语句：{}\n{}".format(db_name, sqls, all_msg)
+        obj = API()
+
+        print(obj.send_mail_for_api(mail_to, 'SQL索引优化', msg))
         print("successful")
     else:
         print('[Error:] git repository is none !!!')
@@ -81,3 +86,6 @@ def main(publish_name, db_name, sqls):
 
 if __name__ == '__main__':
     fire.Fire(main)
+
+### 例如
+### python3.6 /opt/ops_scripts/codo-check/exec_sqladvisor.py shenshuo_server-test shen  '11224;dfsdfsd'
